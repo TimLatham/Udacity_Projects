0a324ee4abd141e02f114d1f81c033df5b0f305c
Tim Latham <lathamtk@yahoo.com>
2017-02-08 18:41:37 -0700

    feat: Add solution
    Updated the return to use a replace feature to solve the issue 
    of losing punctuation present in the prior code.

--- Intro_to_Programming/Stage2/madLibsPlayGameSession5Quiz.py
+++ Intro_to_Programming/Stage2/madLibsPlayGameSession5Quiz.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 08 16:11:34 2017
-
-@author: tim.latham
-"""
-
 # Write code for the function play_game, which takes in as inputs parts_of_speech
-# (a list of acceptable replacement words) and ml_string (a string that 
+# (a list of acceptable replacement words) and ml_string (a string that
 # can contain replacement words that are found in parts_of_speech). Your play_game
 # function should return the joined list replaced, which will have the same structure
 # as ml_string, only that replacement words are swapped out with "corgi", since this
-# program cannot replace those words with user input. 
+# program cannot replace those words with user input.
 
 parts_of_speech  = ["PLACE", "PERSON", "PLURALNOUN", "NOUN"]
 
@@ -19,15 +12,15 @@ test_string = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN
 def word_in_pos(word, parts_of_speech):
     for pos in parts_of_speech:
         if pos in word:
-            return "corgi"
+            return word.replace(pos, "corgi")
     return word
-        
-def play_game(ml_string, parts_of_speech):    
+
+def play_game(ml_string, parts_of_speech):
     replaced = []
     list_string = ml_string.split()
     for i in list_string:
         replaced.append(word_in_pos(i, parts_of_speech))
     sentence = " ".join(replaced)
     return sentence
-    
-print play_game(test_string, parts_of_speech)       
+
+print play_game(test_string, parts_of_speech)
