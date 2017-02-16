15d251bccc71067ec9bb1d9a94a884e1555c5b20
Tim Latham <lathamtk@yahoo.com>
2017-02-15 17:09:04 -0700

    chore: Crawler progress update
    Code has significant development remaining

--- Intro to CS/webCrawler - Intro to CS.py
+++ Intro to CS/webCrawler - Intro to CS.py
@@ -4,6 +4,13 @@ Created on Tue Feb 14 14:52:38 2017
 
 @author: tim.latham
 """
+import time
+
+def time_execution(code):
+    start = time.clock() # start the clock
+    result = eval(code) # evaluate any string as if it is a Python command
+    run_time = time.clock() - start # find the difference in start and end time
+    return result, run_time # return the result of the code and the time taken
 
 def get_next_target(page):
     start_link = page.find('<a href=')
@@ -19,7 +26,7 @@ def get_all_links(page):
     while True:
         url, endpos = get_next_target(page)
         if url:
-            link.append(url)
+            links.append(url)
             page = page[endpos:]
         else:
             break
@@ -33,7 +40,7 @@ def crawl_web(seed):
             content = get_page(page)
             
             
-            union (tocrawl, get_all_links(content)
+            union(tocrawl, get_all_links(content))
             crawled.append(page)
     return crawled
 
@@ -44,21 +51,37 @@ def get_page(url):
     except:
         return ""
     
-'''
+def make_big_index(size):
+    index = []
+    letters = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+    while len(index) < size:
+        word = make_string(letters)
+        add_to_index(index, word, 'fake')
+        for i in range(len(letters) - 1, 0, -1):
+            if letters[i] < 'z':
+                letters[i] = chr(ord(letters[i]) + 1)
+                break
+            else:
+                letters[i] = 'a'
+    return index
+        
+def make_string(p):
+    s = ""
+    for e in p: # for each element in the list p
+        s = s + e # add it to string s
+    return s
+
 def add_to_index(index, keyword, url):
     for entry in index: 
         if entry[0] == keyword: 
             entry[1].append(url) 
             return 
-            
-
-    
     
 index = []
-add_to_index(index, 'udacity', 'http://udacity.com')
-index.append([keyword, [url]])
+add_to_index(index, word, 'fake')
+#index.append([keyword, [url]])
 print index
-'''
+
 
 
 links = get_all_links('http://www.udacity.com/cs101x/index.html')
