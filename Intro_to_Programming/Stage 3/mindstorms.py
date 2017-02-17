59006454235804ba526aa084305f626379a6307c
Tim Latham <lathamtk@yahoo.com>
2017-02-17 09:48:09 -0700

    chore: Load mindstorms in progress
    Drawing shapes using turtle classes

---
+++ Intro_to_Programming/Stage 3/mindstorms.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 08:53:33 2017
+
+@author: tim.latham
+"""
+import turtle
+
+def draw_shapes():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(4)
+    
+    for i in range(36):
+        draw_square(brad)
+        brad.right(10)
+    
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.speed(1.5)
+    
+    draw_circle(angie)
+
+    fred = turtle.Turtle()
+    fred.shape("triangle")
+    fred.color("green")
+    fred.speed(3)    
+    
+    draw_triangle(fred)
+    
+    tim = turtle.Turtle()
+    tim.shape("turtle")
+    tim.color("brown")
+    tim.speed(2)
+    
+    draw_name(tim)
+    
+    window.exitonclick()
+    
+def draw_square(brad):
+    for i in range(4):
+        brad.forward(100) # Move 100 pixels?? forward
+        brad.right(90) # Turn 90 degrees right
+    
+def draw_circle(angie):        
+    angie.circle(100)
+
+def draw_triangle(fred):    
+    for i in range(3):
+        fred.forward(60)
+        fred.left(120)
+
+def draw_name(tim):
+    tim.forward(100)
+    tim.right(180)
+    tim.forward(50)
+    tim.left(90)
+    tim.forward(100)
+        
+draw_shapes()
