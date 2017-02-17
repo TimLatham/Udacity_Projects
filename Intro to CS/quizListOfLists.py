b38120fe88ad624797fe7e7d89a06972a8fb7780
Tim Latham <lathamtk@yahoo.com>
2017-02-17 10:35:22 -0700

    chore: Load quiz list of lists

---
+++ Intro to CS/quizListOfLists.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 10:08:53 2017
+
+@author: tim.latham
+"""
+
+# Define a procedure, total_enrollment,
+# that takes as an input a list of elements,
+# where each element is a list containing
+# three elements: a university name,
+# the total number of students enrolled,
+# and the annual tuition fees.
+
+# The procedure should return two numbers,
+# not a string, 
+# giving the total number of students
+# enrolled at all of the universities
+# in the list, and the total tuition fees
+# (which is the sum of the number
+# of students enrolled times the
+# tuition fees for each university).
+
+udacious_univs = [['Udacity',90000,0]]
+
+usa_univs = [ ['California Institute of Technology',2175,37704],
+              ['Harvard',19627,39849],
+              ['Massachusetts Institute of Technology',10566,40732],
+              ['Princeton',7802,37000],
+              ['Rice',5879,35551],
+              ['Stanford',19535,40569],
+              ['Yale',11701,40500]  ]
+
+def total_enrollment(univs):
+    tuition, enrollment, count = 0, 0, 0
+    while count < len(univs):
+        tuition = tuition + univs[count][1]
+        enrollment = enrollment + (univs[count][1] * univs[count][2])
+        #print tuition, enrollment, count        
+        count += 1
+    return tuition, enrollment
+
+
+print total_enrollment(udacious_univs)
+#>>> (90000,0)
+
+# The L is automatically added by Python to indicate a long
+# number. If you are trying the question in an outside 
+# interpreter you might not see it.
+
+print total_enrollment(usa_univs)
+#>>> (77285,3058581079)
+    
