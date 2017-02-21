819d120863b500be76deb96b34151b50a6de82a4
Tim Latham <lathamtk@yahoo.com>
2017-02-20 21:24:11 -0700

    chore: Load variable practice exercises
    Variable practice exercises from Udacity with my proposed solutions.

---
+++ Intro_to_Programming/Stage 5/iOS/Variables Exercises.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: ## Variables Exercises
+//: In these exercises, you will create variables and constants, and you will handle simple compiler areas that arise when working with them.
+/*:
+### Exercise 1
+
+For each of the following names and types, create a variables or constant that best represents it:
+
+- boilingPointOfWaterInCelsius (Double)
+- gallonsRemainingInTank (Double)
+- naturalLogarithmicBase (Double)
+- ageOfSpecimenInWeeks (Int)
+- studentNickname (String)
+*/
+let boilingPointOfWaterInCelsius = 100.00
+var gallonsRemainingInTank = 2.10
+let naturalLogarithmicBase = 10.0
+var ageOfSpecimenInWeeks = 5
+var studentNickname = "Fritz Alfredo"
+//: ### Exercise 2
+//: Fix the following compiler errors related to assignment, variables, and constants:
+var favoriteLanguage: String = "Swift"
+gallonsRemainingInTank = 2.10
+ageOfSpecimenInWeeks = 32
+let _numberOfGasIcons: Double = gallonsRemainingInTank
+let numberOfPolygonsInModel = 30
+var numberOfPolygonsInThisModel = 32
+var polygonsLeftToDraw = 0
+polygonsLeftToDraw = numberOfPolygonsInModel
+
+//: ### Exercise 3
+//: Fix the following compiler errors related to invalid names:
+var nameOfSchool = "Udacity"
+var classInSchool = "Math"
+let importAuto = "Nissan"
+var myDog = "🐶"
+var yourDog = "🐶"
+
+
+
