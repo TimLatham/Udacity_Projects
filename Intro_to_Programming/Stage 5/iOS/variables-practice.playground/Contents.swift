64f8f68ccac3605e26a3e83b5cdc30f79f72a998
Tim Latham <lathamtk@yahoo.com>
2017-02-20 21:09:31 -0700

    chore: Load variables practice playground
    Sample code from project prework load

---
+++ Intro_to_Programming/Stage 5/iOS/variables-practice.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+// This is a single-line comment.
+/* This is a really long multi-line comment that repeats itself so that it can fit across multiple lines. This is a really long multi-line comment that repeats itself so that it can fit across multiple lines. */
+
+import UIKit
+
+var str = "Hello, playground" // this statement creates a variable
+
+var myJerseyNumber = 5
+
+var myCoolVariable: Character = "a"
+
+var numberOfWheels: Int = 4
+var inchesOfRainfall: Float = 3.5
+var attackRating: Double = 4210.50
+var letterGuessed: Character = "e"
+var secretWord: String = "Barnacles"
+var rainingOutside: Bool = true
