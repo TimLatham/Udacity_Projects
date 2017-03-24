a49976c3b3af28a5e25f49e3b9fa4f68d822b0d4
Tim Latham <lathamtk@yahoo.com>
2017-03-23 20:34:02 -0600

    feat: Added comments

--- DeepLearningND/Project1/demoLiveStream.py
+++ DeepLearningND/Project1/demoLiveStream.py
@@ -10,14 +10,18 @@ def compute_error_for_line_given_points(b, m, points):
     return totalError / float(len(points))
 
 def step_gradient(b_current, m_current, points, learningRate):
+    #starting points for gradients
     b_gradient = 0
     m_gradient = 0
     N = float(len(points))
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
+        # direction with respect to b and m
+        # computing partial derivatives of the error function
         b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
         m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    # update b and m values using the partial derivatives
     new_b = b_current - (learningRate * b_gradient)
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
@@ -38,6 +42,7 @@ def run():
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
     num_iterations = 1000
+    # Step 3 - train model
     print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
     print "Running..."
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
